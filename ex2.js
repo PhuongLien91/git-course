f889bc6606d002f9bccfb74359b1b8371e1f7f6f
Udemy - Lien Nguyen <nguyenphuonglien1102@gmail.com>
2026-06-08 17:04:05 +0700

    ex_javascript cơ ban

---
+++ ex2.js
@@ -0,0 +1,4 @@
+let myName = "Alex";
+myName = "Nagi";
+console.log(myName);
+console.log("const không thể thay đổi giá trị sau khi được gán");
---
+++ ex3.js
@@ -0,0 +1,6 @@
+const Number = 69;
+if (Number > 0)
+{console.log("Giá trị bạn nhập là số dương")}
+else if (Number < 0)
+{console.log("Giá trị bạn nhập là số âm")}
+else {console.log("Giá trị bạn nhập là số 0")}

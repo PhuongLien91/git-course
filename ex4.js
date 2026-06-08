8d65c4afebf5b591d2c5b2f322deb1b7b06825d2
Udemy - Lien Nguyen <nguyenphuonglien1102@gmail.com>
2026-06-08 17:46:24 +0700

    ex js2

---
+++ ex4.js
@@ -0,0 +1,8 @@
+let height = 159;
+let weightIdeal;
+let weightMax;
+let weightMin;
+weightIdeal = (height - 100) * 9 / 10;
+weightMax = (height - 100);
+weightMin = (height - 100) * 8 / 10;
+console.log("Cân nặng lý tưởng của bạn là: " + weightIdeal + "kg," + " cân nặng tối đa của bạn là: " + weightMax + "kg," + " cân nặng tối thiểu của bạn là: " + weightMin + "kg");
